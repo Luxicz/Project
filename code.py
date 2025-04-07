55e179c91769cf374adc78cd20134f6806330fde
Luxicz <luky@kratky.org>
2025-04-07 17:24:21 +0200

    update code.py

--- code.py
+++ code.py
@@ -1,9 +1,19 @@
-print("Hello, this is my program \n\t \f\a\v\b test")
+import os
+os.system('cls' if os.name=='nt' else 'clear') # Command to clear the CLI
 
+list = []
+# print("Hello, this is my program \n\t \f\a\v\b test")
 
-def add(prompt):
-    if prompt == 1:
-        print("asi 1")
-
-
-add(1)
+while True:  
+    print("Write your input:", end=" ")
+    temp = input()
+    if temp == "":
+        del temp
+        break
+    else:
+        list.append(temp)
+        del temp
+if list:
+    print(list)
+    for i in range(len(list)):
+        print(list[i],end=" ")
